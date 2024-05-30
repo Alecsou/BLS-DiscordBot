08c03a101cae6c6b7c098e68e07fedaa033f99cb
Alexandre Soulié <alexandresouliegadg@gmail.com>
2024-05-30 14:38:56 +0200

    config.json pour dev
    Ajout d'un systeme de modification du fichier config.json selon l'environnement

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+fs = require("fs");
 const { Client, IntentsBitField } = require("discord.js");
 const mongoose = require('mongoose');
 const eventHandler = require("./handlers/eventHandler");
@@ -46,8 +47,22 @@ module.exports={
 if (process.argv[2] && process.argv[2] === '-DEV') {
   console.log("DEV ENVIRONNEMENT")
   client.login(process.env.DEVTOKEN);
+  var configGlobalName = "config.json";
+  var devConfigName = "./configs/devconfig.json";
+  var configGlobal = JSON.parse(fs.readFileSync(configGlobalName).toString());
+  var devConfig = JSON.parse(fs.readFileSync(devConfigName).toString());
+  configGlobal.adminServer = devConfig.adminServer;
+  configGlobal.playerServer = devConfig.playerServer;
+  fs.writeFileSync(configGlobalName,JSON.stringify(configGlobal));
 } else {
   console.log("PROD ENVIRONNEMENT")
   client.login(process.env.TOKEN);
+  var configGlobalName = "config.json";
+  var prodConfigName = "./configs/prodconfig.json";
+  var configGlobal = JSON.parse(fs.readFileSync(configGlobalName).toString());
+  var prodConfig = JSON.parse(fs.readFileSync(prodConfigName).toString());
+  configGlobal.adminServer = prodConfig.adminServer;
+  configGlobal.playerServer = prodConfig.playerServer;
+  fs.writeFileSync(configGlobalName,JSON.stringify(configGlobal));
 }
 
